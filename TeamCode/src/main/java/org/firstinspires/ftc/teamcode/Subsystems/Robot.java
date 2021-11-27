9dca9e4fcc9bde06d409ab4a162776985c1bf0b3
Ryan Lin <rlin23205@gmail.com>
2021-11-27 13:09:37 -0800

    Reversed motors to proper orientations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Robot.java
@@ -159,10 +159,10 @@ public class Robot extends Subsystem {
         rearLeftDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("bl");
         rearRightDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("br");
 
-//        frontRightDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-//        rearRightDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeftDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rearLeftDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearRightDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+//        frontLeftDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+//        rearLeftDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         frontLeftDriveMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         frontRightDriveMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
