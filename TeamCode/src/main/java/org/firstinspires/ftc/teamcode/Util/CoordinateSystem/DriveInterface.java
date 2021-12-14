906a5cff29cf980d3c4de5617d2d6ce92fcc7330
Ashwin <arihant2math@gmail.com>
2021-12-14 11:22:55 -0800

    updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/CoordinateSystem/DriveInterface.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/CoordinateSystem/DriveInterface.java
@@ -4,4 +4,14 @@ import org.firstinspires.ftc.teamcode.Subsystems.Drive.Drive;
 
 public class DriveInterface {
 
+    Drive drive;
+
+    public DriveInterface(Drive drive) {
+        this.drive = drive;
+    }
+
+    public void move(Move move) {
+        this.drive.turnByAngle(move.getPower(), move.getAngle()); // Make angle correct
+        this.drive.moveForward(move.getPower(), move.getDistance());
+    }
 }
