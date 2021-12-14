3b1038bdd8bbcd980191f6a7c48479daf86ec87b
Ashwin <arihant2math@gmail.com>
2021-12-13 19:28:27 -0800

    Removed references to TRC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants.java
@@ -4,21 +4,8 @@ package org.firstinspires.ftc.teamcode.Util;
  * This class contains platform independent utility methods. All methods in this class are static.
  * It is not necessary to instantiate this class to call its methods.
  */
-public class TrcUtil {
+public class Constants {
   public static final double INCHES_PER_CM = 0.393701;
   public static final double MM_PER_INCH = 25.4;
   public static final double EARTH_GRAVITATIONAL_CONSTANT = 9.807; // in m/s2
-
-  /**
-   * This interface provides the method to get data of the specified type. This is to replaced the
-   * Supplier interface that Java SDK provides but Android API level 19 does not have.
-   */
-  public interface DataSupplier<T> {
-    /**
-     * This method returns the data of the designated type.
-     *
-     * @return data of the designated type.
-     */
-    T get();
-  }
 }
