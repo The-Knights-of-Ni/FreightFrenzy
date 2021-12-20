7772d16d0d660c932382bf5dca1ddc1033b396f6
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-12-20 15:56:17 -0800

    Secondary path made

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoRedSecondary.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.Subsystems.Drive;
+import org.firstinspires.ftc.teamcode.Util.AllianceColor;
+
+import java.io.IOException;
+
+public class AutoRedSecondary extends Auto {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot robot = null;
+        try {
+            robot = init(AllianceColor.RED);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assert robot != null;
+        waitForStart();
+        int placementLevel;
+        Drive drive = robot.drive;
+
+        placementLevel = getHubLevel(robot.vision);
+        telemetry.addData("Location", placementLevel);
+        telemetry.update();
+
+        // Move in front of the big pole thingy
+        drive.moveForward(7 * mmPerInch);
+        drive.turnRobotByTick(30);
+        drive.moveForward(22 * mmPerInch);
+        sleep(5000);
+
+        // Move back to the warehouse
+        drive.turnRobotByTick(-120);
+        drive.moveForward(50);
+
+        while (opModeIsActive());
+    }
+}
