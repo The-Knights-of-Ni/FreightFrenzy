61adc7b3eb2cfb9a067e9c91c7f245aaba639411
albonec <agbonecchi@gmail.com>
2021-12-30 11:19:22 -0800

    removed unused import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/ScoreThread.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/ScoreThread.java
@@ -4,12 +4,7 @@ import org.firstinspires.ftc.teamcode.Robot;
 
 public class ScoreThread extends Thread implements Runnable{
     private Robot robot;
-    boolean direction;
     int placementLevel;
-    final int RETRACTED = 0;
-    final int BOTTOM = -481;
-    final int MIDDLE = -758;
-    final int TOP = -1292;
 
     public ScoreThread(Robot robot, int placementLevel) {
         this.robot = robot;
