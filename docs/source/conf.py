e396ede0f099dd47bf2fa669e6afef0a49018ecb
Ashwin <arihant2math@gmail.com>
2021-12-30 13:02:36 -0800

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, The Knights of Ni'
 author = 'The Knights of Ni'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.0.0-alpha'
 
 
 # -- General configuration ---------------------------------------------------
