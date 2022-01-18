a038bbfa8465e30672783d4797bf35ae5a5b4435
Ryan Lin <rlin23205@gmail.com>
2022-01-17 17:54:10 -0800

    Added additional debug line cuz i did not want to be confused lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -202,8 +202,7 @@ public class Robot {
             telemetryBroadcast("Status", " vision initializing...");
             vision = new Vision(telemetry, hardwareMap, timer, allianceColor);
         }
-
-
+        telemetryBroadcast("Status", " subsystems initialized...");
     }
 
     public void getGamePadInputs() {
