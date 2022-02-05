c2390bbb83b82241570d7f1fd5558055a801b3ea
Ryan Lin <rlin23205@gmail.com>
2022-02-05 14:03:48 -0800

    Beginnings of secondary auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoRedSecondary.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoRedSecondary.java
@@ -19,15 +19,28 @@ public class AutoRedSecondary extends Auto {
             telemetry.update();
         }
 
-        // Move in front of the big pole thingy
-        drive.moveForward(7 * mmPerInch);
-        drive.turnByAngle(45);
-        drive.moveForward(18 * mmPerInch);
-        sleep(1000);
+        // Move to hub (and start ScoreThread)
+        new ScoreThread(robot, placementLevel).start();
+        double adjustment = 0;
+        switch(placementLevel) {
+            case BOTTOM:
+                adjustment = 1.5;
+                break;
+            case MIDDLE:
+            case TOP:
+                adjustment = 5;
+                break;
+        }
 
-        // Move back to the warehouse
-        drive.turnByAngle(45);
-        drive.moveBackward(74 * mmPerInch);
+        // Move in front of the big pole thingy
+        drive.moveForward(2 * mmPerInch);
+        drive.turnByAngle(30);
+        drive.moveForward((19 + adjustment) * mmPerInch);
+//        sleep(1000);
+//
+//        // Move back to the warehouse
+//        drive.turnByAngle(50);
+//        drive.moveBackward(74 * mmPerInch);
 
     }
 }
