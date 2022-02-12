6eddc9a67053fda381fb7fd13936941acf28b99f
Ashwin Naren <arihant2math@gmail.com>
2022-02-12 13:24:55 -0800

    idk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/LogServerTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Util.LogServer.Log;
+import org.firstinspires.ftc.teamcode.Util.LogServer.LogSync;
+
+import java.io.IOException;
+
+@Autonomous(name = "Log Server Test", group = "Concept")
+public class LogServerTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        LogSync logSync = null;
+        try {
+            logSync = new LogSync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert logSync != null;
+        logSync.logger.add(new Log());
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/LogServer/LogSync.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/LogServer/LogSync.java
@@ -1,18 +1,15 @@
 package org.firstinspires.ftc.teamcode.Util.LogServer;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
 public class LogSync {
-    public LogSync() {
-        try {
-            URI uri = new URI("http://java.sun.com/");
-            URL url = uri.toURL();
-            InputStream in = url.openStream();
-        } catch (IOException | URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
+    public Logger logger = new Logger(9119);
+
+    public LogSync() throws IOException {
+        ServerSocket serverSocket = new ServerSocket();
     }
 }
