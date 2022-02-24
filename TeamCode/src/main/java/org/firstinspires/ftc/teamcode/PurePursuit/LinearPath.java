45ecb4bfe869f87c776b80a33bf2c8484c820da6
Ethan Yun <ethanyun@ohs.stanford.edu>
2022-02-23 17:52:09 -0800

    We have a get current nvvm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PurePursuit/LinearPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PurePursuit/LinearPath.java
@@ -21,10 +21,6 @@ public class LinearPath {
         return path.get(current);
     }
 
-    public Coordinate peek() {
-        return path.get(current);
-    }
-
     public void addCoordinate(Coordinate c) {
         path.add(c);
     }
