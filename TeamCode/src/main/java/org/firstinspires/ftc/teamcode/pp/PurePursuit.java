e123883e0cf8cf42171cac93f9b13b8770c745ad
Ethan Yun <ethanyun@ohs.stanford.edu>
2022-03-05 15:02:26 -0800

    Template for PurePursuit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pp/PurePursuit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pp/PurePursuit.java
@@ -1,7 +1,20 @@
 package org.firstinspires.ftc.teamcode.pp;
 
-public class PurePursuit {
-    Field field;
-    RobotModel robotModel;
-    LinearPath path;
+public class PurePursuit implements Runnable {
+    private Field field;
+    private RobotModel robotModel;
+    private LinearPath path;
+
+    public PurePursuit(Field field, RobotModel robotModel, LinearPath path) {
+        this.field = field;
+        this.robotModel = robotModel;
+        this.path = path;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            // TODO: Shenanigans
+        }
+    }
 }
